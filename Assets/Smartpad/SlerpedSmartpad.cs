e41b686436eef697bf8536af3acc3070200c2cfd
Michel Zimmer <mzimmer@uni-bremen.de>
2016-07-14 18:36:08 +0200

    Provide slerp as part of the API

---
+++ Assets/Smartpad/SlerpedSmartpad.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Smartpad
+{
+	public class SlerpedSmartpad : EventEmitter, EventListener
+	{
+		public EventEmitter Smartpad;
+		public float T = 0.9f;
+
+		private Quaternion Buffer;
+
+		void Start ()
+		{
+			Smartpad.Add (this);
+		}
+
+		public override Quaternion MostRecent ()
+		{
+			return Buffer;
+		}
+
+		public void On (Quaternion q)
+		{
+			Buffer = Quaternion.Slerp (Buffer, q, Time.deltaTime / Time.fixedDeltaTime * T);
+			Emit (Buffer);
+		}
+	}
+}
